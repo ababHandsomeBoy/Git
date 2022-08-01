865c2d5f480917616d1ab2f65ae01133ded00f2c
ChenWei <2104713476@qq.com>
2022-08-01 16:00:43 +0800

    final commit2

--- src/main/java/git/gitDemo.java
+++ src/main/java/git/gitDemo.java
@@ -8,6 +8,7 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git2");
+        System.out.println("push test");
         System.out.println("hot-fix git");
     }
 }
