94aca70abc3f4ffede9cb0d362790939b1bb696f
ChenWei <2104713476@qq.com>
2022-08-01 14:12:06 +0800

    first commit

---
+++ src/main/java/git/gitDemo.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author chenwei
+ * @create 2022-08-01-14:10
+ */
+public class gitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
