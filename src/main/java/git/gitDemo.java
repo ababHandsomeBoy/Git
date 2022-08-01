5e120cc02d8615094dd4ade58cbc50cd0ebdf8d6
ChenWei <2104713476@qq.com>
2022-08-01 14:54:57 +0800

    hot-fix commit2

--- src/main/java/git/gitDemo.java
+++ src/main/java/git/gitDemo.java
@@ -7,6 +7,8 @@ package git;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git4");
         System.out.println("hot-fix git");
+
     }
 }
